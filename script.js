05607285a0b1af42808cccc29c73539ec4fb2cdd
chris fotos <chrisfotos.developer@gmail.com>
2022-08-30 22:29:35 -0400

    fix slider bug

--- script.js
+++ script.js
@@ -1,137 +1,122 @@
-let btn = document.getElementById('consultation-btn')
-let sect = document.getElementById('sect-2')
-let btmsect = document.getElementById('btm-sect')
-let backbtn = document.getElementById('back-btn')
-let cards = Array.from(document.getElementsByClassName('card'))
-let starterSVG = document.getElementById('starter-svg')
-let hustlerSVG = document.getElementById('hustler-svg')
-let mvpSVG = document.getElementById('mvp-svg')
-let starterTitle = document.getElementById('starter-title')
-let hustlerTitle = document.getElementById('hustler-title')
-let MVPTitle = document.getElementById('mvp-title')
-let instruct = document.getElementById('instruct')
-let hustlerDetails = document.getElementById('hustler-details')
-let starterDetails = document.getElementById('starter-details')
-let mvpDetails = document.getElementById('mvp-details')
-let showInstructions = true
+let btn = document.getElementById('consultation-btn');
+let sect = document.getElementById('sect-2');
+let btmsect = document.getElementById('btm-sect');
+let backbtn = document.getElementById('back-btn');
+let cards = Array.from(document.getElementsByClassName('card'));
+let starterSVG = document.getElementById('starter-svg');
+let hustlerSVG = document.getElementById('hustler-svg');
+let mvpSVG = document.getElementById('mvp-svg');
+let starterTitle = document.getElementById('starter-title');
+let hustlerTitle = document.getElementById('hustler-title');
+let MVPTitle = document.getElementById('mvp-title');
+let instruct = document.getElementById('instruct');
+let hustlerDetails = document.getElementById('hustler-details');
+let starterDetails = document.getElementById('starter-details');
+let mvpDetails = document.getElementById('mvp-details');
+let showInstructions = true;
 
+btn.addEventListener('click', (e) => {
+	document.body.style.overflowY = 'hidden';
+	sect.classList.toggle('float-down');
+	btmsect.classList.toggle('float-up');
+});
 
-btn.addEventListener('click', e =>{
-    document.body.style.overflowY = 'hidden'
-    sect.classList.toggle('float-down')
-    btmsect.classList.toggle('float-up')
-})
+backbtn.addEventListener('click', (e) => {
+	sect.classList.toggle('float-down');
+	btmsect.classList.toggle('float-up');
+});
 
-backbtn.addEventListener('click', e =>{
-    sect.classList.toggle('float-down')
-    btmsect.classList.toggle('float-up')
-})
+cards.forEach((c) => {
+	c.addEventListener('click', (e) => {
+		if (e.target.dataset.pkg === 'starter') {
+			Array.from(document.getElementsByClassName('pull-left')).forEach((e) => {
+				e.classList.toggle('pull-left');
+			});
 
+			Array.from(document.getElementsByClassName('pull-right')).forEach((e) => {
+				e.classList.toggle('pull-right');
+			});
 
-cards.forEach(c => {
-    c.addEventListener('click', e=>{
+			Array.from(document.getElementsByClassName('pull-up')).forEach((e) => {
+				e.classList.toggle('pull-up');
+			});
 
+			starterSVG.classList.toggle('pull-right');
+			starterTitle.classList.toggle('pull-left');
+			instruct.classList.add('instruct-fade');
+			setTimeout(() => starterDetails.classList.toggle('pull-up'), 900);
+		}
 
-        if(e.target.dataset.pkg === 'starter'){
-            Array.from(document.getElementsByClassName('pull-left')).forEach(e=> {
-                e.classList.toggle('pull-left')
-            })
+		if (e.target.dataset.pkg === 'hustler') {
+			Array.from(document.getElementsByClassName('pull-left')).forEach((e) => {
+				e.classList.toggle('pull-left');
+			});
 
-            Array.from(document.getElementsByClassName('pull-right')).forEach(e=> {
-                e.classList.toggle('pull-right')
-            })
+			Array.from(document.getElementsByClassName('pull-right')).forEach((e) => {
+				e.classList.toggle('pull-right');
+			});
 
-            Array.from(document.getElementsByClassName('pull-up')).forEach(e=> {
-                e.classList.toggle('pull-up')
-            })
+			Array.from(document.getElementsByClassName('pull-up')).forEach((e) => {
+				e.classList.toggle('pull-up');
+			});
 
+			hustlerSVG.classList.toggle('pull-right');
+			hustlerTitle.classList.toggle('pull-left');
+			instruct.classList.add('instruct-fade');
+			setTimeout(() => hustlerDetails.classList.toggle('pull-up'), 900);
+		}
 
-            starterSVG.classList.toggle('pull-right')
-            starterTitle.classList.toggle('pull-left')
-            instruct.classList.add('instruct-fade')
-            setTimeout(()=>starterDetails.classList.toggle('pull-up'), 900)
-        }
+		if (e.target.dataset.pkg === 'mvp') {
+			Array.from(document.getElementsByClassName('pull-left')).forEach((e) => {
+				e.classList.toggle('pull-left');
+			});
 
-        if(e.target.dataset.pkg === 'hustler'){
-            Array.from(document.getElementsByClassName('pull-left')).forEach(e=> {
-                e.classList.toggle('pull-left')
-            })
+			Array.from(document.getElementsByClassName('pull-right')).forEach((e) => {
+				e.classList.toggle('pull-right');
+			});
 
-            Array.from(document.getElementsByClassName('pull-right')).forEach(e=> {
-                e.classList.toggle('pull-right')
-            })
+			Array.from(document.getElementsByClassName('pull-up')).forEach((e) => {
+				e.classList.toggle('pull-up');
+			});
 
-            Array.from(document.getElementsByClassName('pull-up')).forEach(e=> {
-                e.classList.toggle('pull-up')
-            })
+			mvpSVG.classList.toggle('pull-right');
+			MVPTitle.classList.toggle('pull-left');
+			instruct.classList.add('instruct-fade');
+			setTimeout(() => mvpDetails.classList.toggle('pull-up'), 900);
+		}
+	});
+});
 
-            hustlerSVG.classList.toggle('pull-right')
-            hustlerTitle.classList.toggle('pull-left')
-            instruct.classList.add('instruct-fade')
-            setTimeout(()=>hustlerDetails.classList.toggle('pull-up'), 900)
-        }
+let slider = new Slider(document.getElementsByClassName('sliderize')[0], null, {
+	slideDistance: parseInt(
+		document.getElementsByClassName('card')[0].getBoundingClientRect().width
+	),
+	loopAfter: 3,
+	backButton: document.getElementById('backward'),
+	forwardButton: document.getElementById('forward'),
+});
 
-        if(e.target.dataset.pkg === 'mvp'){
-            Array.from(document.getElementsByClassName('pull-left')).forEach(e=> {
-                e.classList.toggle('pull-left')
-            })
+// c.addEventListener('mouseleave', e=>{
 
-            Array.from(document.getElementsByClassName('pull-right')).forEach(e=> {
-                e.classList.toggle('pull-right')
-            })
+//     if(e.target.dataset.pkg === 'starter'){
+//         starterSVG.classList.toggle('pull-right')
+//         starterTitle.classList.toggle('pull-left')
+//         instruct.classList.toggle('instruct-fade')
+//         starterDetails.classList.toggle('pull-up')
+//     }
 
-            Array.from(document.getElementsByClassName('pull-up')).forEach(e=> {
-                e.classList.toggle('pull-up')
-            })
+//     if(e.target.dataset.pkg === 'hustler'){
+//         hustlerSVG.classList.toggle('pull-right')
+//         hustlerTitle.classList.toggle('pull-left')
+//         instruct.classList.toggle('instruct-fade')
+//         hustlerDetails.classList.toggle('pull-up')
+//     }
 
-            mvpSVG.classList.toggle('pull-right')
-            MVPTitle.classList.toggle('pull-left')
-            instruct.classList.add('instruct-fade')
-            setTimeout(()=>mvpDetails.classList.toggle('pull-up'), 900)
-        }
-    })
-})
+//     if(e.target.dataset.pkg === 'mvp'){
+//         mvpSVG.classList.toggle('pull-right')
+//         MVPTitle.classList.toggle('pull-left')
+//         instruct.classList.toggle('instruct-fade')
+//         mvpDetails.classList.toggle('pull-up')
+//     }
 
-let slider = new Slider(document.getElementsByClassName('sliderize')[0], null, 
-{
-    slideDistance: 500,
-    loopAfter: 5,
-    backButton: document.getElementById('backward'),
-    forwardButton: document.getElementById('forward'),
-})
-
-
-
-
-
-
-
-
-
-
-
-
-    // c.addEventListener('mouseleave', e=>{
-
-    //     if(e.target.dataset.pkg === 'starter'){
-    //         starterSVG.classList.toggle('pull-right')
-    //         starterTitle.classList.toggle('pull-left')
-    //         instruct.classList.toggle('instruct-fade')
-    //         starterDetails.classList.toggle('pull-up')
-    //     }
-
-    //     if(e.target.dataset.pkg === 'hustler'){
-    //         hustlerSVG.classList.toggle('pull-right')
-    //         hustlerTitle.classList.toggle('pull-left')
-    //         instruct.classList.toggle('instruct-fade')
-    //         hustlerDetails.classList.toggle('pull-up')
-    //     }
-
-    //     if(e.target.dataset.pkg === 'mvp'){
-    //         mvpSVG.classList.toggle('pull-right')
-    //         MVPTitle.classList.toggle('pull-left')
-    //         instruct.classList.toggle('instruct-fade')
-    //         mvpDetails.classList.toggle('pull-up')
-    //     }
-
-    // })
+// })
--- slider.js
+++ slider.js
@@ -1,125 +1,152 @@
-// buttons that will slide to specific position
-
 class Slider {
-    constructor(container, pictures, {slideDistance, loopAfter, forwardButton, backButton, autoSlide, setup,} = {}){
+	constructor(
+		container,
+		pictures,
+		{
+			slideDistance,
+			loopAfter,
+			forwardButton,
+			backButton,
+			autoSlide,
+			setup,
+		} = {}
+	) {
+		this.forwardButton = forwardButton;
+		this.backButton = backButton;
+		this.container = container;
+		this.pictures = pictures;
+		this.offset = 0;
+		this.slides = [];
+		this.createPhotoContainer();
 
-        this.forwardButton  = forwardButton
-        this.backButton     = backButton
-        this.container      = container
-        this.pictures       = pictures
-        this.offset         = 0
-        this.slides         = []
-        this.createPhotoContainer()
-        
-        if(this.pictures){
-            this.addPictures()
-            this.addButtonListeners()
-        } else {
-            this.sliderizeElements()
-            this.addButtonListeners()
-        }
+		if (this.pictures) {
+			this.addPictures();
+			this.addButtonListeners();
+		} else {
+			this.sliderizeElements();
+			this.addButtonListeners();
+		}
 
-        if(!slideDistance){ 
-            this.slideDistance = this.container.getBoundingClientRect().width 
-        } else if (slideDistance && loopAfter){
-            this.customSlideDistance = slideDistance
-            this.loopAfter = loopAfter + 1
-        }
+		if (!slideDistance) {
+			console.log('SETTING SLIDE DISTANCE');
+			this.slideDistance = this.container.getBoundingClientRect().width;
+		} else if (slideDistance && loopAfter) {
+			console.log('SETTING SLIDE DISTANCE');
+			this.customSlideDistance = slideDistance;
+			this.loopAfter = loopAfter + 1;
+		}
 
-        if(autoSlide){ 
-            setInterval(this.slideForward.bind(this), autoSlide) 
-        }
+		if (autoSlide) {
+			setInterval(this.slideForward.bind(this), autoSlide);
+		}
 
+		if (setup) {
+			if (typeof setup === 'function') {
+				setup();
+			}
+			if (typeof setup === 'array') {
+				setup.forEach((f) => f());
+			}
+		}
 
-        if(setup){ 
-            if(typeof setup === 'function'){
-                setup()
-            }; 
-            if(typeof setup === 'array'){
-                setup.forEach(f => f())
-        }}
- 
-        window.onresize = () => {
-            this.slideDistance = this.container.getBoundingClientRect().width
-            this.offset = 0
-            this.slides.forEach(s=>{
-                s.style.transform = `translateX(-${this.offset}px)`
-            })
-        }
-    }
+		window.onresize = () => {
+			this.slideDistance = this.container.getBoundingClientRect().width;
+			this.offset = 0;
+			this.slides.forEach((s) => {
+				s.style.transform = `translateX(-${this.offset}px)`;
+			});
+		};
+	}
 
-    createPhotoContainer(){
-        this.container.style.display = 'flex'
-        this.container.style.overflow = 'hidden'
-    }
+	createPhotoContainer() {
+		this.container.style.display = 'flex';
+		this.container.style.overflow = 'hidden';
+	}
 
-    addPictures(){
-        this.pictures.forEach(p => {
-            let img = document.createElement('img')
-            img.src = p
-            img.classList.add('slider_item')
-            this.slides.push(img)
-            this.container.appendChild(img)
-        })
-    }
+	addPictures() {
+		this.pictures.forEach((p) => {
+			let img = document.createElement('img');
+			img.src = p;
+			img.classList.add('slider_item');
+			this.slides.push(img);
+			this.container.appendChild(img);
+		});
+	}
 
-    sliderizeElements(){
-        let children = Array.from(this.container.children)
+	sliderizeElements() {
+		let children = Array.from(this.container.children);
 
-        children.forEach(c => {
-            if(!c.dataset.noSlide){
-                c.classList.add('slider_item')
-                this.slides.push(c)
-                console.log(c, this.slides)
-            }
-        })
-    }
+		children.forEach((c) => {
+			if (!c.dataset.noSlide) {
+				c.classList.add('slider_item');
+				this.slides.push(c);
+			}
+		});
+	}
 
-    slideForward(){
-        
-        function f(s){
-            if(s.customSlideDistance){
-                return {a: s.customSlideDistance, b: s.loopAfter}
-            } else return {a: s.slideDistance, b: s.slides.length}
-        } 
+	slideForward() {
+		console.log(this.offset);
+		function findSlideshowLength(s) {
+			if (s.customSlideDistance) {
+				return {
+					slideDistance: s.customSlideDistance,
+					maximumSlides: s.loopAfter,
+				};
+			} else
+				return {
+					slideDistance: s.slideDistance,
+					maximumSlides: s.slides.length,
+				};
+		}
 
+		if (
+			this.offset <
+			findSlideshowLength(this).slideDistance *
+				findSlideshowLength(this).maximumSlides
+		) {
+			this.offset += findSlideshowLength(this).slideDistance;
+		}
 
-        if(this.offset < (f(this).a * f(this).b)) {
-            this.offset += f(this).a
-        }
+		if (
+			this.offset ===
+			findSlideshowLength(this).slideDistance *
+				findSlideshowLength(this).maximumSlides
+		) {
+			this.offset = 0;
+		}
 
-        if(this.offset === f(this).a * f(this).b) {
-            console.log('gfdsbgdffhghtrfse45wrgtsd')
-            this.offset = 0
-        }
+		this.slides.forEach((s) => {
+			s.style.transform = `translateX(-${this.offset}px)`;
+		});
+	}
 
-        this.slides.forEach(s=>{
-            s.style.transform = `translateX(-${this.offset}px)`
-        })
-    }
+	slideBackward() {
+		console.log(this.offset);
+		console.log('running slideBackward');
+		if (this.offset > 0) {
+			this.offset -= this.slideDistance || this.customSlideDistance;
+			console.log(this.offset);
 
-    slideBackward(){
-        console.log('running slideBackward')
-        if(this.offset > 0) {
-        this.offset -= this.slideDistance
-    
-        this.slides.forEach(s=>{
-            console.log('applying offset')
-            s.style.transform = `translateX(-${this.offset}px)`
-        })}
-    }
+			this.slides.forEach((s) => {
+				console.log('applying offset');
+				s.style.transform = `translateX(-${this.offset}px)`;
+			});
+		}
+		console.log(this.offset);
+	}
 
-    addButtonListeners(){
-        console.log('running', this.forwardButton, this.backButton)
-        if(this.forwardButton){
-            
-            this.forwardButton.addEventListener('click', e => {
-            this.slideForward()
-        })}
+	addButtonListeners() {
+		console.log('running', this.forwardButton, this.backButton);
+		if (this.forwardButton) {
+			this.forwardButton.addEventListener('click', (e) => {
+				this.slideForward();
+			});
+		}
 
-        if(this.forwardButton){
-            this.backButton.addEventListener('click', e => {
-            this.slideBackward()
-        })}
-    }
+		if (this.forwardButton) {
+			this.backButton.addEventListener('click', (e) => {
+				this.slideBackward();
+			});
+		}
+	}
 }
